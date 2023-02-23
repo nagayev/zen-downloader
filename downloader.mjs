a2fe3865c3e696ac0098e8dbeae3ffa32c0d81f1
Marat Nagayev <nagaevmt49@gmail.com>
2023-02-24 01:18:11 +0300

    Fix downloader: `mpd` instead of `m3u8`

--- downloader.mjs
+++ downloader.mjs
@@ -1,31 +1,39 @@
 import cheerio from 'cheerio';
+import fetch from 'node-fetch'
 import fs from 'fs';
 import child_process from 'child_process';
 
 async function main(){
-    if (process.argv.length!==3){
+    if (!(process.argv.length==3 || process.argv.length==4)){
         console.error(`Invalid script usage!`);
-        console.log(`Usage: node downloader.mjs video_url`);
+        console.log(`Usage: node downloader.mjs video_url [saveAs]`);
+        console.log(`Default saveAs value is zen.mp4`);
         process.exit(1);
     }
     const url = process.argv[2];
+    const saveAs = process.argv[3]?process.argv[3]:"zen.mp4";
     console.log(`url is ${url}`);
     console.log(`Loading html document...`);
-    child_process.execSync(`curl ${url} > tmp.html`);
-    const $ = cheerio.load(fs.readFileSync('tmp.html').toString('utf-8'));
-    console.log(`Searching m3u8...`);
+    const response = await fetch(url,{method:'POST'});
+    const body = await response.text();
+    const $ = cheerio.load(body);
+    console.log(`Searching mpd...`);
     const contentDiv = $('.content');
     let result = contentDiv.next().html();
-    const m3u8 = result.indexOf('m3u8');
-    let pos = m3u8;
+    const mpd = result.indexOf('mpd');
+    let pos = mpd;
+    if (pos===-1){
+        console.log(`Coudn't find mpd url`);
+        process.exit(2);
+    }
     while (result[pos]+result[pos-1]+result[pos-2]+result[pos-3]!=="ptth"){
         pos--;
     }
     console.log('Done!');
-    const m3u8_url = result.slice(pos-3,m3u8+4);
-    console.log(`m3u8 url is ${m3u8_url}`);
-    console.log(`Calling ffmpeg...`);
-    child_process.execSync(`ffmpeg -i ${m3u8_url} -c copy -bsf:a aac_adtstoasc "output.mp4"`);
-    console.log(`Saved as output.mp4`);
+    const mpd_url = result.slice(pos-3,mpd+3);
+    console.log(`mpd url is ${mpd_url}`);
+    console.log(`Downloading using youtube-dl...`);
+    child_process.execSync(`youtube-dl ${mpd_url} --output ${saveAs}`);
+    console.log(`Saved as ${saveAs}`);
 }
 main();
